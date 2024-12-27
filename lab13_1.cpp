79967e430895b81dff4498e26db34a329d5dc557
ZiV99 <71887602+VitaminB7@users.noreply.github.com>
2024-12-27 20:20:39 +0700

    lab13_1.cpp Completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,33 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int N,double b[]){
+    double sum=0,Amean=0,SD=0,Gmean=1,Hmean=0,max=0,min=0;
+
+    for(int i=0;i<N;i++){sum+=a[i];}
+    Amean =sum/N;
+    b[0] = Amean;
+    
+    for(int i=0;i<N;i++){SD+=pow(a[i]-Amean,2);}
+    SD= sqrt(SD/N);
+    b[1] = SD;
+    
+    for(int i=0;i<N;i++){Gmean*=a[i];}
+    Gmean = pow(Gmean,1.0/N);
+    b[2] = Gmean;
+    
+    for(int i=0;i<N;i++){Hmean+=(1/a[i]);}
+    Hmean = N/Hmean;
+    b[3] = Hmean;
+    
+    max=a[0],min=a[0];
+    for(int i=0;i<N;i++){if(max<a[i])max=a[i]; if(min>a[i])min=a[i];}
+    b[4]=max;
+    b[5]=min;    
+    
+    
+    
+    
+    
+}
