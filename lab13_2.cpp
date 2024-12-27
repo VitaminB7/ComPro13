1b4d6bb56c189d1c4a90f0a020a28d68b8a6fdbc
ZiV99 <71887602+VitaminB7@users.noreply.github.com>
2024-12-27 20:23:06 +0700

    lab13_2.cpp Completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 #include <cmath>
 using namespace std;
 
@@ -21,3 +21,37 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+
+void updateImage(bool im[][M],int s,int x,int y){
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if (sqrt(pow(i-x,2)+pow(j-y,2))<=s-1) im[i][j]=1;
+        }
+    }
+}
+
+void showImage(const bool im[][M]){
+    
+    cout<<"------------------------------------------------------------------------"<<'\n';
+    
+    for(int i=0;i<N;i++){
+        
+         cout<<"|";
+        
+        for(int j=0;j<M;j++){
+        
+        if (im[i][j] == 0) cout<<" ";
+        else if (im[i][j] == 1) cout<<"*";
+        
+        }   
+        cout<<"|";
+        cout<<'\n';
+    }
+    cout<<"------------------------------------------------------------------------"<<'\n';
+    
+}
+
+
+
